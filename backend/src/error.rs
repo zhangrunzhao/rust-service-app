7ac5f73e668e667678b9d8712e0b500e7c64e2df
zhangrunzhao <zhangrunzhao@bytedance.com>
2024-03-14 22:27:58 +0800

    feat: 引入日志相关工具和环境变量，搭建开发环境下的 db 环境

--- backend/src/error.rs
+++ backend/src/error.rs
@@ -6,29 +6,29 @@ pub type Result<T> = core::result::Result<T, Error>;
 
 #[derive(Debug)]
 pub enum Error {
-    // -- Config
-    ConfigMissingEnv(&'static str),
+	// -- Config
+	ConfigMissingEnv(&'static str),
 
-    // -- Modules
-    Model(model::Error),
+	// -- Modules
+	Model(model::Error),
 }
 
 // region:    --- Froms
 impl From<model::Error> for Error {
-    fn from(value: model::Error) -> Self {
-        Self::Model(value)
-    }
+	fn from(value: model::Error) -> Self {
+		Self::Model(value)
+	}
 }
 
 // endregion: --- Froms
 
 impl Display for Error {
-    fn fmt(
-        &self,
-        fmt: &mut core::fmt::Formatter<'_>,
-    ) -> core::result::Result<(), core::fmt::Error> {
-        write!(fmt, "{self:?}")
-    }
+	fn fmt(
+		&self,
+		fmt: &mut core::fmt::Formatter<'_>,
+	) -> core::result::Result<(), core::fmt::Error> {
+		write!(fmt, "{self:?}")
+	}
 }
 
 impl std::error::Error for Error {}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,6 +5,8 @@ use crate::web::routes_login;
 use model::ModelManager;
 use std::net::SocketAddr;
 use tower_cookies::CookieManagerLayer;
+use tracing::info;
+use tracing_subscriber::EnvFilter;
 
 use axum::{middleware, Router};
 use web::{mw_auth::mw_ctx_resolve, mw_res_map::mw_response_map, routes_static};
@@ -19,30 +21,42 @@ mod log;
 mod model;
 mod web;
 
+pub mod _dev_utils;
+
 // endregion: --- Modules
 
 #[tokio::main] // 它会使 main 异步执行
 async fn main() -> Result<()> {
-    // Initialize ModelManager.
-    let mm = ModelManager::new().await?;
+	tracing_subscriber::fmt()
+		.without_time()
+		.with_target(false)
+		.with_env_filter(EnvFilter::from_default_env())
+		.init();
 
-    // 洋葱模型，写在后面的函数越早执行
-    let routes_all = Router::new()
-        .merge(routes_login::routes())
-        // 这个中间件主要是用于处理返回到客户端中的 res body
-        .layer(middleware::map_response(mw_response_map))
-        .layer(middleware::from_fn_with_state(mm.clone(), mw_ctx_resolve))
-        // 引入 CookieManagerLayer 后，所有的路由中的函数的第一个参数都是 cookie，参考 api_login_handler
-        // 或者是：https://docs.rs/tower-cookies/latest/tower_cookies/
-        .layer(CookieManagerLayer::new())
-        .fallback_service(routes_static::serve_dir());
+	// -- 仅为了测试
+	_dev_utils::init_dev().await;
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+	// Initialize ModelManager.
+	let mm = ModelManager::new().await?;
 
-    axum::Server::bind(&addr)
-        .serve(routes_all.into_make_service())
-        .await
-        .unwrap();
+	// 洋葱模型，写在后面的函数越早执行
+	let routes_all = Router::new()
+		.merge(routes_login::routes())
+		// 这个中间件主要是用于处理返回到客户端中的 res body
+		.layer(middleware::map_response(mw_response_map))
+		.layer(middleware::from_fn_with_state(mm.clone(), mw_ctx_resolve))
+		// 引入 CookieManagerLayer 后，所有的路由中的函数的第一个参数都是 cookie，参考 api_login_handler
+		// 或者是：https://docs.rs/tower-cookies/latest/tower_cookies/
+		.layer(CookieManagerLayer::new())
+		.fallback_service(routes_static::serve_dir());
 
-    Ok(())
+	let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+
+	info!("{:<12} - {addr}\n", "LISTENING");
+	axum::Server::bind(&addr)
+		.serve(routes_all.into_make_service())
+		.await
+		.unwrap();
+
+	Ok(())
 }
