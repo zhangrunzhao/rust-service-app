4c878f8e3509b40645b83518b41489b9a04a5aee
zhangrunzhao <zhangrunzhao@bytedance.com>
2024-03-31 21:15:59 +0800

    feat: 优化静态文件服务能力，将页面路由控制权交由前端

--- backend/src/web/routes_static.rs
+++ backend/src/web/routes_static.rs
@@ -3,14 +3,13 @@ use axum::{
     http::StatusCode,
     routing::{any_service, MethodRouter},
 };
-use tower_http::services::ServeDir;
+use tower_http::services::{ServeDir, ServeFile};
 
 const WEB_FOLDER: &str = "../frontend/dist";
+const WEB_FILE: &str = "../frontend/dist/index.html";
 
+// https://github.com/tokio-rs/axum/blob/main/examples/static-file-server/src/main.rs
 pub fn serve_dir() -> MethodRouter {
-    async fn handle_404() -> (StatusCode, &'static str) {
-        (StatusCode::NOT_FOUND, "Resource not found")
-    }
-
-    any_service(ServeDir::new(WEB_FOLDER).not_found_service(handle_404.into_service()))
+    // 页面路由交由前端进行处理，服务端仅需要把静态文件服务处理好即可
+    any_service(ServeDir::new(WEB_FOLDER).not_found_service(ServeFile::new(WEB_FILE)))
 }
